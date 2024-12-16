426bfa3da11a2bf80bf917387f01a054024349e6
zyoung-herazy <wlwl10000@naver.com>
2024-12-16 16:39:55 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String last = "??!";
+		
+		String s = sc.next();
+		System.out.println(s + last);
+	}
+
+}
